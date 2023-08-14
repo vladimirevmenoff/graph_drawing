1c1757c4e821191b4ed570160727ff78868d38e0
Vladimir Evmeno <vladimir.evmenov.jr@gmail.com>
2023-08-14 21:07:42 +0300

    add working circle

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from graphics import *
+
 def main():
-	print('Hello World!')
+	win = GraphWin("My Circle", 100, 100)
+	c = Circle(Point(50,50), 10)
+	c.draw(win)
+	win.getMouse() # Pause to view result
+	win.close()
 
 if __name__ == '__main__':
 	main()
