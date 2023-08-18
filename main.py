fbb86e3773e2ba58e85ab0cc1ce5c74683f13c2d
Vladimir Evmeno <vladimir.evmenov.jr@gmail.com>
2023-08-18 22:07:46 +0300

    first working algo

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from graphics import *
+from springs import spring
 
 def main():
-	win = GraphWin("My Circle", 100, 100)
-	c = Circle(Point(50,50), 10)
-	c.draw(win)
+	win = GraphWin("My Circle", 800, 800)
+
+	g = [[1, 2], [0, 2], [0, 1, 3, 4], [2], [2, 5], [4]]
+	coords = spring(g)
+
+	for i in range(len(coords)):
+		coords[i][0] += 20
+		coords[i][1] += 20 
+
+		coords[i][0] *= 20
+		coords[i][1] *= 20
+
+	for i in coords:
+		c = Circle(Point(i[0], i[1]), 1)
+		c.draw(win)
+
+	for u in range(len(g)):
+		for v in g[u]:
+			line = Line(Point(coords[u][0], coords[u][1]), Point(coords[v][0], coords[v][1]))
+			line.draw(win)
+
+
 	win.getMouse() # Pause to view result
 	win.close()
 
---
+++ springs.py
@@ -0,0 +1,79 @@
+def spring(g):
+	from random import uniform
+
+	n = len(g)
+	inf = +1000000
+
+	dp = [[inf for i in range(n)] for j in range(n)]
+
+	for v in range(n):
+
+		dp[v][v] = 0
+		for u in g[v]:
+			dp[v][u] = 1
+			dp[u][v] = 1 
+	        
+	#Floyd-Warshall
+	D = 0
+	K = 10 
+	for i in range(n):
+		for j in range(n):
+			for k in range(n):
+				dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+			
+			D = max(D, dp[i][j])		
+	 
+	L = n / D
+
+	k = [[0 for i in range(n)] for j in range(n)]
+	l = [[0 for i in range(n)] for j in range(n)]
+
+	for i in range(n):
+		for j in range(n):
+			if i == j:
+				continue
+			k[i][j] = K / (dp[i][j] ** 2)
+			l[i][j] = L * dp[i][j]
+	print(L)
+
+	coords = [[uniform(0, n), uniform(0, n)] for i in range(n)]
+
+	def en():
+		E = 0
+		for i in range(n):
+			for j in range(i + 1, n):
+				E += 0.5 * k[i][j] * ((coords[i][0] - coords[j][0]) ** 2 +
+						      (coords[i][1] - coords[j][1]) ** 2 + l[i][j] ** 2 -
+						       2 * l[i][j] * (((coords[i][0] - coords[j][0]) ** 2 + (coords[i][1] - coords[j][1]) ** 2) ** 0.5))
+
+		return E
+
+	def grad(m):
+		eps = 0.0001
+		
+		E = en()
+		coords[m][0] += eps
+		E_x = en()
+		coords[m][0] -= eps
+		coords[m][1] += eps
+		E_y = en()
+		coords[m][1] -= eps
+
+		return ((E_x - E) / eps, (E_y - E) / eps)
+
+	def grad_step(m):
+		eps = 0.0001
+		
+		
+		gr = grad(m)
+		while((gr[0] ** 2 + gr[1] ** 2) ** 0.5 > 0.05):
+			coords[m][0] -= eps * gr[0]
+			coords[m][1] -= eps * gr[1]
+			gr = grad(m)
+
+
+	for j in range(5):
+		for i in range(n):
+			grad_step(i)
+
+	return coords
